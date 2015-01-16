bab051303f63946debf31ec6f9d0e597e85cf358
pumba30 <pumba303030@yandex.ru>
2015-01-16 14:09:13 +0200

    v. 0.1

--- src/NameCompare.java
+++ src/NameCompare.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
  * Created by pumba30 on 16.01.2015.
  */
 public class NameCompare implements Comparator<Mountain> {
+
     @Override
     public int compare(Mountain one, Mountain two) {
         return one.getName().compareTo(two.getName());
